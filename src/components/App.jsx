0f7320f31f526f06452e48caeb7191cf3056663b
Jasurbek Kalandarov <62233209+jkalandarov@users.noreply.github.com>
2020-05-22 20:07:24 +0500

    Add delete function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
-import notes from '../notes';
-
-const createNote = note=>{
-    return (
-        <Note 
-            key={note.key}
-            title={note.title}
-            content={note.content}
-        />
-    );
-}
+import CreateArea from './CreateArea';
 
 
 const App = ()=> {
-    return (<div>
-    <Header />
-    {notes.map(createNote)}
+    const [notes, setNotes] = useState([]);
 
-    <Footer />
+    function addNote(newNote) {
+        setNotes(prevNotes=>{
+            return [...prevNotes, newNote];
+        })
+    }
+
+    function deleteNote(id){
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index)=> index !== id);
+        });
+    }
+
+    return (
+    <div>
+        <Header />
+        <CreateArea onAdd={addNote} />
+        {notes.map((noteItem, index)=>{
+            return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote} />
+        })}
+        <Footer />
     </div>
     );
 }
